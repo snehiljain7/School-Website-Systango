4c350ab85025a30fddff05827bc8f9f51a24b90d
snehiljain7 <44917768+snehiljain7@users.noreply.github.com>
2020-06-22 00:53:27 +0530

    Students and Institute Info Added

--- EskoolyApp/views.py
+++ EskoolyApp/views.py
@@ -137,7 +137,7 @@ class StudentsAddView(CreateView):
 @admin_only
 def StudentsView(request):
     return render(request, 'EskoolyApp/viewstudents.html', {'students': Students.objects.all()})
-    
+
 
 @method_decorator(decoratorss, name='dispatch')
 class StudentsDetailView(DetailView):
@@ -156,7 +156,8 @@ class StudentsDeleteView(DeleteView):
     def get_success_url(self):
         return reverse('view_students')
 
-
+@login_required(login_url='login')
+@admin_only
 def StudentAdmissionLetter(request):
     if request.method == 'GET':
         query= request.GET.get('q')
@@ -179,6 +180,8 @@ def StudentAdmissionLetter(request):
     else:
         return render(request, 'EskoolyApp/admission_letter.html')
 
+@login_required(login_url='login')
+@admin_only
 def PrintAdmissionLetter(request):
     if request.method == 'GET':
         query= request.GET.get('q')
@@ -200,104 +203,3 @@ def PrintAdmissionLetter(request):
 
     else:
         return render(request, 'EskoolyApp/print_admission_letter.html')
-
-# def admin_login(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(username=username, password=password)
-#         if user:
-#             if user.is_active:
-#                 login(request,user)
-#                 return HttpResponseRedirect(reverse('index'))
-#             else:
-#                 return HttpResponse("Your account was inactive.")
-#         else:
-#             print("Someone tried to login and failed.")
-#             print("They used username: {} and password: {}".format(username,password))
-#             return HttpResponse("Invalid login details given")
-#     else:
-#         return render(request, 'EskoolyApp/login.html', {})
-# @login_required
-# def userLogout(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('index'))
-#
-#
-# def register(request):
-#     return render(request,'EskoolyApp/register.html')
-#
-# def registerStudent(request):
-#     registered=False
-#     if request.method=='POST':
-#         var_studentForm=studentForm(request.POST)
-#         var_studentAddForm=studentAddForm(request.POST)
-#         if var_studentForm.is_valid() and var_studentAddForm.is_valid():
-#             studentprimary=var_studentForm.save()
-#             studentprimary.set_password(studentprimary.password)
-#             studentprimary.save()
-#             studentAdd=var_studentAddForm.save(commit=False)
-#             studentAdd.student=studentprimary
-#             studentAdd.save()
-#             registered=True
-#     else:
-#         var_studentForm=studentForm()
-#         var_studentAddForm=studentAddForm()
-#     return render(request,'EskoolyApp/registerStudent.html',{'var_studentForm':var_studentForm,'var_studentAddForm':var_studentAddForm,'registered':registered})
-#
-#
-# def registerTeacher(request):
-#     registered=False
-#     if request.method=='POST':
-#         var_teacherForm=teacherForm(request.POST)
-#         var_teacherAddForm=teacherAddForm(request.POST)
-#         if var_teacherForm.is_valid() and var_teacherAddForm.is_valid():
-#             teacherprimary=var_teacherForm.save()
-#             teacherprimary.set_password(teacherprimary.password)
-#             teacherprimary.save()
-#             teacherAdd=var_teacherAddForm.save(commit=False)
-#             teacherAdd.teacher=teacherprimary
-#             teacherAdd.save()
-#             registered=True
-#     else:
-#         var_teacherForm=teacherForm()
-#         var_teacherAddForm=teacherAddForm()
-#     return render(request,'EskoolyApp/registerTeacher.html',{'var_teacherForm':var_teacherForm,'var_teacherAddForm':var_teacherAddForm,'registered':registered})
-#
-# def userLogin(request):
-#     invalidlogin=False
-#     if request.method=='POST':
-#         username=request.POST.get('username')
-#         password=request.POST.get('password')
-#         user=authenticate(username=username,password=password)
-#         if user:
-#             if user.is_active:
-#                 login(request,user)
-#                 return HttpResponseRedirect(reverse('index'))
-#             else:
-#                 return HttpResponse('Account not active')
-#         else:
-#             invalidlogin=True
-#             return redirect('/EskoolyApp/login/')
-#     else:
-#         return render(request,'EskoolyApp/login.html',{'invalidlogin':invalidlogin})
-#
-# @login_required
-# def dashboard(request):
-#     try:
-#         current=Student.objects.get(student=request.user)
-#     except Student.DoesNotExist:
-#         current=Teacher.objects.get(teacher=request.user)
-#     if current.is_student:
-#         return redirect('/studentDash/')
-#     else:
-#         return redirect('/teacherDash/')
-#     return render(request,'EskoolyApp/dashboard.html')
-#
-#
-#
-# def studentDash(request):
-#     return render(request,'EskoolyApp/studentDash.html')
-#
-# def teacherDash(request):
-#     return render(request,'EskoolyApp/teacherDash.html')
